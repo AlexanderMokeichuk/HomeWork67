00aa036aa29e639ec1f14fd8afdcaad0c328ed61
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-02-22 18:07:39 +0600

    Initial commit!

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className={"min-vh-100 bg-black d-flex justify-content-center align-items-center"}>
+      <div className={"text-white"}>Home Work</div>
+    </div>
+  );
+}
+
+export default App;
